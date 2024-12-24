1bb940d103859cdfe8a905787c54ab34ac94e67c
Arthur Alves <arthurnalves.pro@gmail.com>
2024-12-24 18:11:42 -0300

    add: minimum reproducible example for coverage flakiness on multiprocessing

---
+++ test_coverage.py
@@ -0,0 +1,9 @@
+import multiprocessing
+
+def complex_math_verification():
+    assert 1 + 1 == 2
+
+def test_foo():
+    with multiprocessing.Pool(processes=1) as pool:
+        pool.apply(complex_math_verification)
+    pool.join()
